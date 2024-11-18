22354f09911fe39eae502eb76b22eb7609f67302
SmoCloud <psn.power@yahoo.com>
2024-11-18 12:52:43 -0500

    Separated edit button from delete button (overlapping buttons caused delete confirm message to pop up when clicking edit button).

--- index.php
+++ index.php
@@ -141,7 +141,7 @@
 <!DOCTYPE html>
 <html lang='en'>
 <head>
-  <title>Basic RPG - Character Sheet Management - Version 3.4.2</title>
+  <title>Basic RPG - Character Sheet Management - Version 3.5.2</title>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="stylesheet" href="styles.css" type="text/css"/>
